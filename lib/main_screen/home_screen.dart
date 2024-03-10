b621ab2568f36e13de56af9136fbd05ea561f7f3
Jose Galvez <jlgalvez77@gmail.com>
2024-03-10 19:52:54 +0100

    Dark Mode

---
+++ lib/main_screen/home_screen.dart
@@ -0,0 +1,67 @@
+import 'package:adaptive_theme/adaptive_theme.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool isDarkMode = false;
+
+  void getThemeMode() async {
+    final savedThemeMode = await AdaptiveTheme.getThemeMode();
+    if (savedThemeMode == AdaptiveThemeMode.dark) {
+      setState(() {
+        isDarkMode = true;
+      });
+    } else {
+      setState(() {
+        isDarkMode = false;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    getThemeMode();
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Card(
+          child: SwitchListTile(
+            title: const Text('Cambiar Tema'),
+            secondary: Container(
+              height: 30,
+              width: 30,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: isDarkMode ? Colors.white : Colors.black,
+              ),
+              child: Icon(
+                isDarkMode ? Icons.nightlight_round : Icons.wb_sunny_rounded,
+                color: isDarkMode ? Colors.black : Colors.white,
+              )
+            ),
+            value: isDarkMode,
+            onChanged: (value) {
+              setState(() {
+                isDarkMode = value;
+              });
+              if (value) {
+                AdaptiveTheme.of(context).setDark();
+              } else {
+                AdaptiveTheme.of(context).setLight();
+              }
+            },
+          ),
+        ),
+      )
+    );
+  }
+}
